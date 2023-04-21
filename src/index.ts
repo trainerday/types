3d792c63d68958e54f455ad4d05806bb8275b44b
AlexanderKiselev <manyvasa29@gmail.com>
2023-04-21 16:56:14 +0500

    Added sort types. upd version to 0.0.6

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { TARGET_TYPES } from './garmin'
 // common
 export { SegmentSoundEffectType } from './trainderday/SoundEffects';
 export { SportType } from './trainderday/SportTypes';
+export { SortType } from './trainderday/SortTypes';
 
 // Activity
 export { ActivitySourceType } from './trainderday/Activity';
---
+++ src/trainderday/SortTypes.ts
@@ -0,0 +1,11 @@
+export enum SortType {
+  NAME_ASC = 'name-asc',
+  NAME_DESC = 'name-desc',
+  CREATED_ASC = 'created-asc',
+  CREATED_DESC = 'created-desc',
+  INSERTED_ASC = 'inserted-asc',
+  INSERTED_DESC = 'inserted-desc',
+  TSS_ASC = 'tss-asc',
+  TSS_DESC = 'tss-desc',
+  LIST_ORDER = 'list-order'
+}
