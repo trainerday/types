167a3abfead8da8d038561d03b9c68cfdae6dc1a
AlexanderKiselev <manyvasa29@gmail.com>
2025-01-23 15:50:23 +0500

    Added new types of sound effects. upd version to 1.1.0

--- src/trainderday/SoundEffects.ts
+++ src/trainderday/SoundEffects.ts
@@ -1,7 +1,26 @@
 export enum SegmentSoundEffectType {
   NONE,
+  /** @deprecated No longer used in the app */
   UP,
+  /** @deprecated No longer used in the app */
   DOWN,
+  /** @deprecated No longer used in the app */
   BEEPS_UP,
-  BEEPS_DOWN
+  /** @deprecated No longer used in the app */
+  BEEPS_DOWN,
+
+  /** Use a long signal. About 100% volume */
+  SINGLE_BEEP,
+
+  /** Use 3 short and 1 long signals. About 32% volume */
+  FOUR_BEEPS_LOW_VOLUME,
+
+  /** Use 3 short and 1 long signals. About 56% volume */
+  FOUR_BEEPS_MID_VOLUME,
+
+  /** Use 3 short and 1 long signals. About 100% volume */
+  FOUR_BEEPS_HIGH_VOLUME,
+
+  /** Use 3 short and 1 long signals. About 20%, 50%, 80%, 100% volume */
+  FOUR_BEEPS_PROGRESSIVE_VOLUME
 }
